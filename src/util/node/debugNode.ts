db2d4e244ce1902e9dd259dc5906d6009053161e
David <mattsson.david@gmail.com>
2025-06-11 22:28:34 +0200

    Add support to print undefined or null nodes for easier debugging

--- src/util/node/debugNode.ts
+++ src/util/node/debugNode.ts
@@ -3,5 +3,7 @@
  * @param {Node} node The node to debug
  * @returns {string} The node debug info
  */
-export const debugNode = (node: Node): string =>
-    `(${node.nodeType}) ${node.nodeName} => ${node.nodeValue}`;
+export const debugNode = (node: Node | undefined | null): string =>
+    node
+        ? `(${node.nodeType}) ${node.nodeName} => ${node.nodeValue}`
+        : '(?) ? => ?';
