6af6df225ce613e1b7113f922e9364775d08106a
David <mattsson.david@gmail.com>
2025-06-19 22:56:39 +0200

    Add missing type declaration for treePrintNode

--- src/util/node/debug/treePrintNode.ts
+++ src/util/node/debug/treePrintNode.ts
@@ -6,7 +6,7 @@ import { debugNode } from './debugNode.ts';
  * @param {Node|null} node The node to print
  * @param {number} depth Tree depth
  */
-export const treePrintNode = (node: Node | null, depth = 0): void => {
+export const treePrintNode = (node: Node | null, depth: number = 0): void => {
     if (!node) return;
     console.log(`${'\t'.repeat(depth)}${debugNode(node)}`);
     node.childNodes.forEach((child): void => treePrintNode(child, depth + 1));
