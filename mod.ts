2c299a350f911279ceea31c3ec115fc4d2405c56
David Mattsson <mattsson.david@gmail.com>
2025-09-04 23:21:06 +0200

    Add the css tagged template to mod

--- mod.ts
+++ mod.ts
@@ -1,3 +1,4 @@
+export { css } from './src/css.ts';
 export { html, type TemplateResult } from './src/html.ts';
 export { render } from './src/render.ts';
 export { renderToString } from './src/renderToString.ts';
