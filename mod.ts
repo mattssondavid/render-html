869522ab61f7bd92d95346c2bf49e7a69a2f4274
David Mattsson <mattsson.david@gmail.com>
2025-09-04 23:22:38 +0200

    Add serializeHTMLfragment to mod

--- mod.ts
+++ mod.ts
@@ -2,3 +2,4 @@ export { css } from './src/css.ts';
 export { html, type TemplateResult } from './src/html.ts';
 export { render } from './src/render.ts';
 export { renderToString } from './src/renderToString.ts';
+export { serializeHTMLfragment } from './src/serialization/serializeHTMLfragment.ts';
