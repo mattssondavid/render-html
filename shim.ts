f30d18b822884e8a7d05682edd711acadf3e23f8
David Mattsson <mattsson.david@gmail.com>
2026-04-01 23:57:22 +0200

    Add node support

--- shim.ts
+++ shim.ts
@@ -1,1 +1,13 @@
-export { createShimDom } from './src/server/shim/shim-dom.ts';
+import { createShimDom as shim } from './src/server/shim/shim-dom.ts';
+
+// @ts-ignore Deno global is not available in Node/tsc for node
+const isDeno = typeof Deno !== 'undefined';
+
+const { JSDOM } = isDeno
+    ? // @ts-ignore npm: specifier is not recognized by tsc for node support
+      await import('npm:jsdom@28.0.0')
+    : await import('jsdom');
+
+export function createShimDom(): Promise<void> {
+    return shim(JSDOM);
+}
--- src/server/shim/shim-dom.ts
+++ src/server/shim/shim-dom.ts
@@ -1,4 +1,5 @@
-export async function createShimDom(): Promise<void> {
+// deno-lint-ignore no-explicit-any
+export async function createShimDom(JSDOM: any): Promise<void> {
     if (typeof globalThis.self === 'undefined') {
         // @ts-ignore Gurantee that `self` exist
         (globalThis as Window & typeof globalThis).self = globalThis;
@@ -14,9 +15,6 @@ export async function createShimDom(): Promise<void> {
     // This require to work as stand-alone import for any Deno environment,
     // so using npm-import. Dynamic import is to not import JSDOM if the shim
     // is not needed.
-    // deno-lint-ignore no-import-prefix
-    const module = await import('npm:jsdom@28.0.0');
-    const { JSDOM } = module.default || module;
     const baseHTML = `<!DOCTYPE html><html><head></head><body></body></html>`;
     const { window } = new JSDOM(baseHTML, { pretendToBeVisual: true });
 
